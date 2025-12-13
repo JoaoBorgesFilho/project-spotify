7477859e8b8653687ecac06895ab90362cb50c9a
JoaoBorgesFilho <kira.ryuuku10@gmail.com>
2025-12-13 13:34:55 -0300

    feat: estrutura inicial do app com layout inspirado no Spotify, componentização e dados mockados

---
+++ src/assets/database/artists.js
@@ -0,0 +1,126 @@
+export const artistArray = [
+  {
+    id: 1,
+    image: "https://picsum.photos/id/1011/300/300",
+    name: "Metal Nobre",
+    banner: "https://picsum.photos/id/1011/1200/400",
+  },
+  {
+    id: 2,
+    image: "https://picsum.photos/id/1012/300/300",
+    name: "Oficina G3",
+    banner: "https://picsum.photos/id/1012/1200/400",
+  },
+  {
+    id: 3,
+    image: "https://picsum.photos/id/1013/300/300",
+    name: "Banda Freedom",
+    banner: "https://picsum.photos/id/1013/1200/400",
+  },
+  {
+    id: 4,
+    image: "https://picsum.photos/id/1014/300/300",
+    name: "Anderson Freire",
+    banner: "https://picsum.photos/id/1014/1200/400",
+  },
+
+  {
+    id: 5,
+    image: "https://picsum.photos/id/1015/300/300",
+    name: "Metal Nobre",
+    banner: "https://picsum.photos/id/1015/1200/400",
+  },
+  {
+    id: 6,
+    image: "https://picsum.photos/id/1016/300/300",
+    name: "Oficina G3",
+    banner: "https://picsum.photos/id/1016/1200/400",
+  },
+  {
+    id: 7,
+    image: "https://picsum.photos/id/1018/300/300",
+    name: "Banda Freedom",
+    banner: "https://picsum.photos/id/1018/1200/400",
+  },
+  {
+    id: 8,
+    image: "https://picsum.photos/id/1019/300/300",
+    name: "Anderson Freire",
+    banner: "https://picsum.photos/id/1019/1200/400",
+  },
+
+  {
+    id: 9,
+    image: "https://picsum.photos/id/1020/300/300",
+    name: "Metal Nobre",
+    banner: "https://picsum.photos/id/1020/1200/400",
+  },
+  {
+    id: 10,
+    image: "https://picsum.photos/id/1021/300/300",
+    name: "Oficina G3",
+    banner: "https://picsum.photos/id/1021/1200/400",
+  },
+  {
+    id: 11,
+    image: "https://picsum.photos/id/1022/300/300",
+    name: "Banda Freedom",
+    banner: "https://picsum.photos/id/1022/1200/400",
+  },
+  {
+    id: 12,
+    image: "https://picsum.photos/id/1023/300/300",
+    name: "Anderson Freire",
+    banner: "https://picsum.photos/id/1023/1200/400",
+  },
+
+  {
+    id: 13,
+    image: "https://picsum.photos/id/1024/300/300",
+    name: "Metal Nobre",
+    banner: "https://picsum.photos/id/1024/1200/400",
+  },
+  {
+    id: 14,
+    image: "https://picsum.photos/id/1025/300/300",
+    name: "Oficina G3",
+    banner: "https://picsum.photos/id/1025/1200/400",
+  },
+  {
+    id: 15,
+    image: "https://picsum.photos/id/1026/300/300",
+    name: "Banda Freedom",
+    banner: "https://picsum.photos/id/1026/1200/400",
+  },
+  {
+    id: 16,
+    image: "https://picsum.photos/id/1027/300/300",
+    name: "Anderson Freire",
+    banner: "https://picsum.photos/id/1027/1200/400",
+  },
+
+  {
+    id: 17,
+    image: "https://picsum.photos/id/1028/300/300",
+    name: "Metal Nobre",
+    banner: "https://picsum.photos/id/1028/1200/400",
+  },
+  {
+    id: 18,
+    image: "https://picsum.photos/id/1029/300/300",
+    name: "Oficina G3",
+    banner: "https://picsum.photos/id/1029/1200/400",
+  },
+  {
+    id: 19,
+    image: "https://picsum.photos/id/1030/300/300",
+    name: "Banda Freedom",
+    banner: "https://picsum.photos/id/1030/1200/400",
+  },
+  {
+    id: 20,
+    image: "https://picsum.photos/id/1031/300/300",
+    name: "Anderson Freire",
+    banner: "https://picsum.photos/id/1031/1200/400",
+  },
+];
